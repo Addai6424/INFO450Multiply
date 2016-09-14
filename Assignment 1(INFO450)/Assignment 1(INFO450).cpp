1569236df51dc35c436e6c305568d578a363a1c9
Addai6424 <francisaddai@hotmail.com>
2016-09-13 21:18:16 -0700

    Commit1

---
+++ Assignment 1(INFO450)/Assignment 1(INFO450).cpp
@@ -0,0 +1,26 @@
+// Assignment 1(INFO450).cpp : Defines the entry point for the console application.
+//
+
+// Assignment 1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include<iostream>
+using namespace std;
+int main(void)
+{
+	int a;
+	int b;
+	cout << "Multiplication table:" << endl
+		<< "  0\t1\t2\t3\t4\t5\t6\t7\t8\t9" << endl;
+	for (int a = 0; a < 11; a++)
+	{
+		cout << a << "=";
+		for (int b = 0; b < 10; b++)
+		{
+			cout << b * a << '\t';
+		}
+		cout << endl;
+	}
+	return 0;
+}
